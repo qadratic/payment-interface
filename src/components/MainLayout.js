e613cc4042a9d7efbecea7005a90ab488851712a
ripudaman <ripudaman.kaushik@yahoo.in>
2020-06-28 21:24:30 +0530

    layout and added material ui

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function MainLayout() {
 	return (
-		<div>
-
+		<div style={styles.container} >
+			<div  >stepper</div>
+			<div>screen</div>
 		</div>
 	)
+}
+
+const styles = {
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		border: 'solid black 1px',
+	},
 }
